49faeee95e280f1e55d22239dcf1451d5ddd1eb0
zeyy-gif <arslanzeynep007@gmail.com>
2026-09-23 17:21:13 +0200

    deuxième cours (Models et migrations)

--- app/Models/Place_user.php
+++ app/Models/Place_user.php
@@ -4,21 +4,17 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Table;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
-#[Table('users')]
-class User extends Authenticatable
+#[Table('place_user', timestamps: false )]
+class Place_user extends Model
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
+    
     /**
      * Get the attributes that should be cast.
      *
--- app/Models/Places.php
+++ app/Models/Places.php
@@ -4,17 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Table;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
-#[Table('users')]
-class User extends Authenticatable
+#[Table('places')]
+class Places extends Model
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
